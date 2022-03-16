e8447096ccae683f13f11c6031ba494b7f9d1aa5
Andruby <zhaomeng@daojia-inc.com>
2022-03-16 20:01:03 +0800

    备案号增加链接

--- resources/views/mero/layout/footer_copyright.blade.php
+++ resources/views/mero/layout/footer_copyright.blade.php
@@ -1,7 +1,7 @@
 @section('footer_copyright')
     <div class="site-info" style="padding-top: 15px; padding-bottom: 15px;">
         <div class="container">
-            Copyright © {{ config('deep_blog.copyright') }}. All Rights Reserved {{ config('deep_blog.beian_number') }}
+            Copyright © {{ config('deep_blog.copyright') }}. All Rights Reserved <a href="https://beian.miit.gov.cn/" target="_blank">{{ config('deep_blog.beian_number') }}</a>
         </div><!-- .container -->
     </div><!-- .site-info -->
 @endsection
